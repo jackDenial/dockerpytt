91ace41279f6cfc3637651beb37699f391123664
jackDenial <jatin.bhalla@atmecs.com>
2018-04-10 06:35:37 -0400

    second

--- simpleapp.py
+++ simpleapp.py
@@ -10,7 +10,7 @@ start = int(round(time.time()))
 @app.route("/")
 def hello_world():
 
-    return "Hello world from Distelli & Docker!"
+    return "Hello world from bhalla & jatin!"
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python simpleapp.py -p ")
